90b52d4d49674e7ac51ad5b78f217214b6096a81
Nabijon Nazarov <nabijonskijnazarov@gmail.com>
2026-09-12 18:31:32 +0800

    Added findByIdempotencyKey to TransactionRepository

--- src/main/java/com/example/fintech_simulator/repository/TransactionRepository.java
+++ src/main/java/com/example/fintech_simulator/repository/TransactionRepository.java
@@ -1,6 +1,7 @@
 package com.example.fintech_simulator.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
 
@@ -8,4 +9,6 @@ import com.example.fintech_simulator.entity.Transaction;
 
 public interface TransactionRepository extends CrudRepository<Transaction, String> {
     List<Transaction> findByFromCardOrToCardOrderByCreatedAtDesc(String fromCard, String toCard);
+
+    Optional<Transaction> findByIdempotencyKey(String idempotencyKey);
 }
