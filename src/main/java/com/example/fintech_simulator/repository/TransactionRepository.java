365dffe3122091b0915de0def2706f8cb3a71a3b
Nabijon Nazarov <nabijonskijnazarov@gmail.com>
2026-09-14 11:55:04 +0800

    Switched TransactionRepository to JpaRepository to get saveAndFlush

--- src/main/java/com/example/fintech_simulator/repository/TransactionRepository.java
+++ src/main/java/com/example/fintech_simulator/repository/TransactionRepository.java
@@ -3,11 +3,11 @@ package com.example.fintech_simulator.repository;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.fintech_simulator.entity.Transaction;
 
-public interface TransactionRepository extends CrudRepository<Transaction, String> {
+public interface TransactionRepository extends JpaRepository<Transaction, String> {
     List<Transaction> findByFromCardOrToCardOrderByCreatedAtDesc(String fromCard, String toCard);
 
     Optional<Transaction> findByIdempotencyKey(String idempotencyKey);
